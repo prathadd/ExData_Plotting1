3481e7f83aba0caca8a6089df207dd65d4fd0bd2
Pratha Jhawar <38035905+prathadd@users.noreply.github.com>
2018-04-21 12:40:46 +0530

    Update Plot3.R

--- Plot3.R
+++ Plot3.R
@@ -2,4 +2,4 @@
 plot(X[,1], as.numeric(as.character(X[,6])), type = "l", ylim = c(0,40), xlab = "Days", ylab = "Energy Sub metering")
 lines(X[,1], as.numeric(as.character(X[,7])), col = "red")
 lines(X[,1], as.numeric(as.character(X[,8])), col = "blue")
-## Find PNG format plot in "figure" file.
+
