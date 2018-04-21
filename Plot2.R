a769585e7bcd727d5a85da2055f22c6eca5e0c3d
Pratha Jhawar <38035905+prathadd@users.noreply.github.com>
2018-04-21 12:51:20 +0530

    Update Plot2.R

--- Plot2.R
+++ Plot2.R
@@ -1,3 +1,5 @@
 ## Pls check out file - " Initial steps" to get an idea on how this worked
 Plot2 <- plot(X[,1], X[,2], xlab = "Days",ylab = "Global Active Power (kW)", type = "l")
-
+# copy plot2 to png device, named as "plot2.png"
+dev.copy(png, file="Plot2.png", width=480, height=480)
+dev.off()
