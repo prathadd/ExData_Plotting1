efb0bab806a3eba4b421fef3c4c2a47526a1338d
Pratha Jhawar <38035905+prathadd@users.noreply.github.com>
2018-04-21 12:52:01 +0530

    Update Plot3.R

--- Plot3.R
+++ Plot3.R
@@ -2,4 +2,7 @@
 plot(X[,1], as.numeric(as.character(X[,6])), type = "l", ylim = c(0,40), xlab = "Days", ylab = "Energy Sub metering")
 lines(X[,1], as.numeric(as.character(X[,7])), col = "red")
 lines(X[,1], as.numeric(as.character(X[,8])), col = "blue")
+# copy plot3 to png device, named as "plot2.png"
+dev.copy(png, file="Plot3.png", width=480, height=480)
+dev.off()
 
