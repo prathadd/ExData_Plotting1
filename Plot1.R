21502ee80572ef7baf2ceedc77f8d615667ad4e5
Pratha Jhawar <38035905+prathadd@users.noreply.github.com>
2018-04-21 12:35:17 +0530

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -1,1 +1,3 @@
+## Pls check out file - " Initial steps" to get an idea on how this worked
 Plot1 <- hist(as.numeric(as.character(X[,2])), main = "Global Active Power", xlab = "Global Active Power (kW)", ylab = "Frequency", col = "red")
+## Find PNG format plot in "figure" file.
