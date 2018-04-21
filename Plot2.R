a0368beecd5f9a5ef9539388e0d611bfd7dd9d93
Pratha Jhawar <38035905+prathadd@users.noreply.github.com>
2018-04-21 12:40:22 +0530

    Update Plot2.R

--- Plot2.R
+++ Plot2.R
@@ -1,3 +1,3 @@
 ## Pls check out file - " Initial steps" to get an idea on how this worked
 Plot2 <- plot(X[,1], X[,2], xlab = "Days",ylab = "Global Active Power (kW)", type = "l")
-## Find PNG format plot in "figure" file.
+
