a4aaa4fc24a2e9f8d48f931b5d99aefe5f3c7efc
Pratha Jhawar <38035905+prathadd@users.noreply.github.com>
2018-04-21 12:50:41 +0530

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -1,3 +1,5 @@
 ## Pls check out file - " Initial steps" to get an idea on how this worked
 Plot1 <- hist(as.numeric(as.character(X[,2])), main = "Global Active Power", xlab = "Global Active Power (kW)", ylab = "Frequency", col = "red")
-
+# copy plot1 to png device, named as "plot2.png"
+dev.copy(png, file="Plot1.png", width=480, height=480)
+dev.off()
