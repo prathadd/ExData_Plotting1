be2f79986e21a2bcda556aa5e2094fc7b7908e10
Pratha Jhawar <38035905+prathadd@users.noreply.github.com>
2018-04-21 13:59:38 +0530

    Update Plot3.R

--- Plot3.R
+++ Plot3.R
@@ -1,9 +1,7 @@
 ## Pls check out file - " Initial Steps" to get an idea on how this worked
-plot(X[,1], as.numeric(as.character(X[,6])), type = "l", ylim = c(0,40), xlab = "Days", ylab = "Energy Sub metering")
-lines(X[,1], as.numeric(as.character(X[,7])), col = "red")
-lines(X[,1], as.numeric(as.character(X[,8])), col = "blue")
-legend('topright', lty=1, c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lwd=2.5, col=c("black", "red", "blue"), bty = "n")
-# copy plot3 to png device, named as "plot2.png"
-dev.copy(png, file="Plot3.png", width=480, height=480)
+png("Plot3.png", width=480, height=480)
+plot(x=hpc_2days$Date_Time, y=hpc_2days$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
+lines(x=hpc_2days$Date_Time, y=hpc_2days$Sub_metering_2, type="l", col="red")
+lines(x=hpc_2days$Date_Time, y=hpc_2days$Sub_metering_3, type="l", col="blue")
+legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=1, col=c("black", "red", "blue"))
 dev.off()
-
