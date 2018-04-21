d50c7355c6b24a2f2ef83173f6766d6d4e119038
Pratha Jhawar <38035905+prathadd@users.noreply.github.com>
2018-04-21 13:57:38 +0530

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -1,5 +1,4 @@
 ## Pls check out file - " Initial steps" to get an idea on how this worked
-Plot1 <- hist(as.numeric(as.character(X[,2])), main = "Global Active Power", xlab = "Global Active Power (kW)", ylab = "Frequency", col = "red")
-# copy plot1 to png device, named as "plot2.png"
-dev.copy(png, file="Plot1.png", width=480, height=480)
+png("Plot1.png", width=480, height=480)
+Plot1 <- hist(hpc_2days$Global_active_power, main = "Global Active Power", xlab = "Global Active Power (kW)", ylab = "Frequency", col = "red")
 dev.off()
