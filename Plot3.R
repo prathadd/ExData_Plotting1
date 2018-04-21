b2807024f608ac8f86673e63f48cdd96d439ef8b
Pratha Jhawar <38035905+prathadd@users.noreply.github.com>
2018-04-21 12:37:04 +0530

    Create Plot3.R

---
+++ Plot3.R
@@ -0,0 +1,5 @@
+## Pls check out file - " Initial Steps" to get an idea on how this worked
+plot(X[,1], as.numeric(as.character(X[,6])), type = "l", ylim = c(0,40), xlab = "Days", ylab = "Energy Sub metering")
+lines(X[,1], as.numeric(as.character(X[,7])), col = "red")
+lines(X[,1], as.numeric(as.character(X[,8])), col = "blue")
+## Find PNG format plot in "figure" file.
