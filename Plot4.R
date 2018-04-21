0e3e44cadb895eb4a76764704dc28f3f0659a464
Pratha Jhawar <38035905+prathadd@users.noreply.github.com>
2018-04-21 12:43:59 +0530

    Create Plot4.R

---
+++ Plot4.R
@@ -0,0 +1,14 @@
+## Pls check out file - " Initial steps" to get an idea on how this worked
+par(mfrow = c(2,2))
+par(mar=c(2,2,2,2))
+par(mgp = c(1,0.5,0))
+
+plot(X[,1], X[,2], xlab = "Days",ylab = "Global Active Power (kW)", type = "l")
+
+plot(X[,1], as.numeric(as.character(X[,4])), xlab = "Days",ylab = "Voltage", type = "l")
+
+plot(X[,1], as.numeric(as.character(X[,6])), type = "l", ylim = c(0,40), xlab = "Days", ylab = "Energy Sub metering")
+lines(X[,1], as.numeric(as.character(X[,7])), col = "red")
+lines(X[,1], as.numeric(as.character(X[,8])), col = "blue")
+
+plot(X[,1], as.numeric(as.character(X[,3])), xlab = "Days",ylab = "Global Reactive Power", type = "l")
